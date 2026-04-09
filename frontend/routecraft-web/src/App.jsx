77d97ed98c9f07c67effe9105d8c97ac360fda2b
Randon Jordan <randon.jordan23@gmail.com>
2026-04-09 15:25:48 -0700

    Fix case-sensitive import for NavBar

--- frontend/routecraft-web/src/App.jsx
+++ frontend/routecraft-web/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Navbar";
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
 import Software from "./pages/Software";
